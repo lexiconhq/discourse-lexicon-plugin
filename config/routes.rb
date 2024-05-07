cf3a111c3f78602689d11be4c2c016cb2b3afd83
Switzer <stephenmulyadi97@gmail.com>
2024-05-07 14:01:24 +0700

    change default route lexicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 DiscourseLexiconPlugin::Engine.routes.draw do
-  post '/subscribe' => 'expo_pn#subscribe'
-  post '/delete_subscribe' => 'expo_pn#unsubscribe'
+  post 'push_notifications/subscribe' => 'expo_pn#subscribe'
+  post 'push_notifications/delete_subscribe' => 'expo_pn#unsubscribe'
 
   post '/delete_user' => 'delete_user#delete'
 end
