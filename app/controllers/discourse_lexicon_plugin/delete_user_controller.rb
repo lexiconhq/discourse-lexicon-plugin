dd725289b2742050dfae2fa94798d41856782bde
Switzer <stephenmulyadi97@gmail.com>
2024-05-07 10:54:03 +0700

    add new controller for delete user api

---
+++ app/controllers/discourse_lexicon_plugin/delete_user_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module DiscourseLexiconPlugin
+  class DeleteUserController < ::ApplicationController
+    requires_plugin DiscourseLexiconPlugin
+
+    def delete_posts(user, batch_size = 20)
+      Reviewable.where(created_by_id: user.id).delete_all
+
+      user.posts
+          .order('post_number desc')
+          .limit(batch_size)
+          .each { |p| DiscourseLexiconPlugin::PostDestroyer.new(user, p).destroy }
+    end
+
+    def delete_account(user)
+      DiscourseLexiconPlugin::UserDestroyer.new(user).destroy(user)
+    end
+
+    def delete
+      password = params.require(:password)
+      user = current_user
+      return render json: { error: 'user not found' } unless user.present?
+      return render json: { error: 'an admin cannot delete its own account.' } if user.admin?
+      return render json: { error: 'wrong password' } unless user.confirm_password?(password)
+
+      delete_posts(user)
+
+      delete_account(user)
+
+      render json: { message: 'success' }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DiscourseLexiconPlugin::Engine.routes.draw do
-    post '/subscribe' => "expo_pn#subscribe"
-    post '/delete_subscribe' => "expo_pn#unsubscribe"
+  post '/subscribe' => 'expo_pn#subscribe'
+  post '/delete_subscribe' => 'expo_pn#unsubscribe'
+
+  post '/delete_user' => 'delete_user#delete'
 end
